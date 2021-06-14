b77edc41e56a2138e85610a6366ce189d0515a72
kena <kena.e8ac99@gmail.com>
2021-06-14 09:32:48 +0900

    test: Fix  to point to the correct file

---
+++ test/src/hash_concepts.cpp
@@ -0,0 +1,35 @@
+#include "hash/hash_concepts.hpp"
+
+#include <cstddef>
+#include <cstdint>
+
+#include <boost/ut.hpp>
+
+int main()
+{
+  using namespace boost::ut;
+
+  "available_as_byte"_test =
+    []()
+    {
+      expect(constant<reki::available_as_byte<std::byte>>);
+
+      expect(constant<reki::available_as_byte<std::int8_t>>);
+
+      expect(constant<reki::available_as_byte<char>>);
+
+      expect(constant<reki::available_as_byte<unsigned char>>);
+    };
+
+  "unavailable_as_byte"_test =
+    []()
+    {
+      expect(constant<!reki::available_as_byte<bool>>);
+
+      expect(constant<!reki::available_as_byte<int>>);
+
+      expect(constant<!reki::available_as_byte<float>>);
+
+      expect(constant<!reki::available_as_byte<std::string>>);
+    };
+}
