313ce3d5da64fdfe0fdc8deefbe3565fd8225e98
kena <kena.e8ac99@gmail.com>
2021-06-14 14:08:49 +0900

    test: Fix a test

--- test/src/hash_bytes.cpp
+++ test/src/hash_bytes.cpp
@@ -3,6 +3,7 @@
 #include <bits/hash_bytes.h>
 
 #include <boost/ut.hpp>
+#include <cstddef>
 
 int main()
 {
@@ -84,5 +85,88 @@ int main()
       auto std_hash_fnv = std::_Fnv_hash_bytes(text.data(), text.size(), 1);
 
       expect(eq(my_hash_fnv, std_hash_fnv));
+    };
+
+  "std::byte[3]"_test =
+    []()
+    {
+      constexpr std::byte test[3]{std::byte{0}, std::byte{1}, std::byte{2}};
+
+      constexpr auto size = sizeof(std::byte) * 3;
+
+      constexpr auto my_hash = reki::hash_bytes(std::span{test}, 1);
+
+      auto std_hash = std::_Hash_bytes(test, size, 1);
+
+      expect(eq(my_hash, std_hash));
+
+      constexpr auto my_hash_fnv = reki::fnv_hash_bytes(std::span{test}, 1);
+
+      auto std_hash_fnv = std::_Fnv_hash_bytes(test, size, 1);
+
+      expect(eq(my_hash_fnv, std_hash_fnv));
+    };
+
+  "array<std::byte, 3>"_test =
+    []()
+    {
+      constexpr std::array<std::byte, 3> test{std::byte{0}, std::byte{1},
+                                              std::byte{2}};
+
+      constexpr auto size = sizeof(decltype(test));
+
+      constexpr auto my_hash = reki::hash_bytes(std::span{test}, 1);
+
+      auto std_hash = std::_Hash_bytes(&test, size, 1);
+
+      expect(eq(my_hash, std_hash));
+
+      constexpr auto my_hash_fnv = reki::fnv_hash_bytes(std::span{test}, 1);
+
+      auto std_hash_fnv = std::_Fnv_hash_bytes(&test, size, 1);
+
+      expect(eq(my_hash_fnv, std_hash_fnv));
+    };
+
+  "int[3]"_test =
+    []()
+    {
+      constexpr int test[3]{0, 1, 2};
+
+      constexpr auto my_hash = reki::hash_bytes(std::span{test}, 1);
+
+      expect(constant<my_hash != 0>);
+
+      constexpr auto my_hash_fnv = reki::fnv_hash_bytes(std::span{test}, 1);
+
+      expect(constant<my_hash_fnv != 0>);
+    };
+
+  "array<int, 3>"_test =
+    []()
+    {
+      constexpr std::array<int, 3> test{0, 1, 2};
+
+      constexpr auto my_hash = reki::hash_bytes(std::span{test}, 1);
+
+      expect(constant<my_hash != 0>);
+
+      constexpr auto my_hash_fnv = reki::fnv_hash_bytes(std::span{test}, 1);
+
+      expect(constant<my_hash_fnv != 0>);
+    };
+
+  "tuple"_test =
+    []()
+    {
+      constexpr std::tuple<int, double, std::byte> value{1, 2.0, std::byte{3}};
+
+      constexpr auto my_hash = reki::hash_bytes(value, 1);
+
+      expect(constant<my_hash != 0>);
+
+      constexpr auto my_hash_fnv = reki::fnv_hash_bytes(value, 1);
+
+      expect(constant<my_hash_fnv != 0>);
     };
 }
