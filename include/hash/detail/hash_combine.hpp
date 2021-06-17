7fd868f84a03c1cbc917df4f12f786e3aac6638a
kena <kena.e8ac99@gmail.com>
2021-06-17 18:07:18 +0900

    feat: Add 64-bit hash_combine

--- include/hash/detail/hash_combine.hpp
+++ include/hash/detail/hash_combine.hpp
@@ -6,6 +6,13 @@ namespace reki::detail
 {
   constexpr void hash_combine(std::size_t& seed, std::size_t value) noexcept
   {
-    seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    if constexpr (sizeof(std::size_t) == 4)
+    {
+      seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
+    else
+    {
+      seed ^= value + 0x9e3779b97f4a7c15LLU + (seed << 12) + (seed >> 4);
+    }
   }
 }
